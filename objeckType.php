21c7e0be42f408ec89edf2c01772a0e54cc5c516
paozanwa22 <fausanwadi@gmail.com>
2021-05-05 11:04:43 -0700

    update 1

--- objeckType.php
+++ objeckType.php
@@ -1,27 +1,30 @@
 <?php 
-class People {
-    public $judul, $penerbit, $penulis, $harga;
-    public function __construct( $judul, $penerbit, $penulis, $harga ) {
-        $this->judul = $judul;
-        $this->penulis = $penulis;
-        $this->harga = $harga;
-        $this->penerbit = $penerbit;
-    }   
-    public function ulala () {
-        return "$this->judul, $this->penulis";
-    }
-}
-class AmbilSemua {
-    public function semua ( People $wadah ) {
-        $str = "{$wadah->ulala()} | $wadah->penerbit, (Rp. $wadah->harga)";
-        return $str;
-    }
-}
-$buku = new People("naruto", "kishi", "oskar", 3000);
-$bukuGambar = new People("lifeTo", "wadi", "fausan", 3000);
-echo $buku->ulala(); echo "<hr>";
-$infoProduk = new AmbilSemua();
-echo $infoProduk->semua($bukuGambar);
 
+class Produk {
+  public $judul,$penulis,$penerbit,$harga;
+  public function __construct($judul = "judul",$penulis = "penulis",$penerbit = "penerbit",$harga = 0) {
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+  }
 
-?>
+  public function getLabel() {
+    return "$this->penulis, $this->penerbit";
+  }
+}
+
+class CetakInfoProduk {
+  public function cetak( Produk $produ ) { //objec dengan tipe dada sendiri. (Produk = tipe data, $produk = Objec)
+    $str = "{$produ->judul} | {$produ->getLabel()} (Rp. {$produ->harga})";
+    return $str;
+  }
+}
+
+$produk1 = new Produk("Naruto","Masashi Kishimoto","Shonen Jump", 30000);
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1->cetak($produk1);
+
