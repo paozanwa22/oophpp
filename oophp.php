5edc60ddc7372ef93b3d8102bb7584e02b7e9848
paozanwa22 <fausanwadi@gmail.com>
2021-05-05 11:50:41 -0700

    inheritence-problem.php

--- oophp.php
+++ oophp.php
@@ -1,30 +1,50 @@
 <?php 
-// Object Type.php
+// inheritence Problem.php
 class Produk {
-  public $judul,$penulis,$penerbit,$harga;
-  public function __construct($judul = "judul",$penulis = "penulis",$penerbit = "penerbit",$harga = 0) {
+  public 
+    $judul = "judul",
+    $penulis = "penulis",
+    $penerbit = "penerbit",
+    $harga = 0,
+    $halaman = 0,
+    $waktu = 0,
+    $tipe = 0;
+
+  public function __construct($judul,$penulis,$penerbit,$harga,$halaman,$waktu,$tipe) {
     $this->judul = $judul;
     $this->penulis = $penulis;
     $this->penerbit = $penerbit;
     $this->harga = $harga;
+    $this->halaman = $halaman;
+    $this->waktu = $waktu;
+    $this->tipe = $tipe;
   }
 
   public function getLabel() {
     return "$this->penulis, $this->penerbit";
   }
-}
 
-class CetakInfoProduk {
-  public function cetak( Produk $produ ) {
-    $str = "{$produ->judul} | {$produ->getLabel()} (Rp. {$produ->harga})";
+  
+  public function getInfoLengkap() {
+    $str = "{$this->judul} {$this->getLabel()} {$this->harga}";
+    if( $this->tipe == "buku" ) {
+      $str .= " - {$this->halaman} halaman.";
+    } else if( $this->tipe == "games" ) {
+      $str .= " ~ {$this->waktu} Jam.";
+    }
     return $str;
   }
 }
 
-$produk1 = new Produk("Naruto","Masashi Kishimoto","Shonen Jump", 30000);
-$produk2 = new Produk("Life","Kuskus","Wolan", 40000);
-echo "Komik : " . $produk1->getLabel();
-echo "<br>";
-$infoProduk1 = new CetakInfoProduk();
-echo "Games : " . $infoProduk1->cetak($produk2);
+class CetakInfoProduk {
+  public function cetak( Produk $produ ) {
+    $str = "{$produ->judul} | {$produ->getLabel()} (Rp. {$produ->harga}) {$produ->halaman} {$produ->waktu}";
+    return $str;
+  }
+}
 
+$produk1 = new Produk("Naruto","Masashi Kishimoto","Shonen Jump", 30000, 100, 0, "buku");
+$produk2 = new Produk("PUBG","Masashi Kishimoto","Shonen Jump", 30000, 0, 50, "games");
+$coba = new CetakInfoProduk();
+echo $coba->cetak($produk1); echo "<br>";
+echo $produk2->getInfoLengkap();
